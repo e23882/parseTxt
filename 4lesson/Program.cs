75e047c5305a81ddd431b91871cba0144fb1e6b7
LeoYang <e23882@hotmail.com>
2019-05-05 17:36:46 +0800

    first version commit

---
+++ 4lesson/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _4lesson
+{
+    #region Property
+    public class data
+    {
+        public string key { get; set; }
+        public string vlaue { get; set; }
+    }
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<data> list = new List<data>();
+            StreamReader str = new StreamReader(@"D:\key.txt");
+
+            string item = string.Empty;
+            while ((item = str.ReadLine())!=null)
+            {
+
+                if (item.IndexOf("raw") != -1)
+                {
+                    list.Add(
+                        new data
+                        {
+                            key = item.Substring(item.IndexOf("raw") + 4, item.IndexOf("=") - (item.IndexOf("raw") + 4)),
+                            vlaue = item.Substring(item.IndexOf("=") + 1, item.Length - (item.IndexOf("=") + 1))
+                        });    
+                }
+            }
+            str.Close();
+
+
+            //read log.txt
+            StreamReader strInput = new StreamReader(@"D:\key.txt");
+
+            while ((item = str.ReadLine()) != null)
+            {
+                //phase data n do something
+            }
+        }
+    }
+}
